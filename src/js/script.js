6d621e4b6e71b5e4952780484b04acbe1bd9410a
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2022-07-28 04:01:03 +0300

    попробовал JS

--- src/js/script.js
+++ src/js/script.js
@@ -30,3 +30,53 @@ function closeOnClick() {
   hamb.classList.remove("active");
   body.classList.remove("noscroll");
 }
+
+
+// Получаем объекты ссылок меню и объекты секций
+const linkHome = document.getElementById('link_home');
+const linkAbout = document.getElementById('link_about');
+const linkProject = document.getElementById('link_project');
+const linkFooter = document.getElementById('link_footer');
+
+const home = document.getElementById('home');
+const about = document.getElementById('about');
+const project = document.getElementById('project');
+const contact = document.getElementById('contact');
+const footer = document.getElementById('footer');
+
+window.addEventListener('scroll', myScroll);
+
+function myScroll() {
+  let windowTop = window.pageYOffset;
+
+  let homeHeight = home.scrollHeight;
+  let aboutHeight = about.scrollHeight;
+  let projectHeight = project.scrollHeight;
+  let contactHeight = contact.scrollHeight;
+
+  if (windowTop < homeHeight) {
+    linkHome.classList.add('link');
+  }
+  else {
+    linkHome.classList.remove('link');
+  }
+
+  if (homeHeight - 5 < windowTop && windowTop < homeHeight + aboutHeight -5) {
+    linkAbout.classList.add('link');
+  }
+  else {
+    linkAbout.classList.remove('link');
+  }
+  if (homeHeight + aboutHeight -5 < windowTop && windowTop < homeHeight + aboutHeight + projectHeight -5) {
+    linkProject.classList.add('link');
+  }
+  else {
+    linkProject.classList.remove('link');
+  }
+  if (homeHeight + aboutHeight + projectHeight -5 < windowTop && windowTop < homeHeight + aboutHeight + projectHeight + contactHeight -5) {
+    linkFooter.classList.add('link');
+  }
+  else {
+    linkFooter.classList.remove('link');
+  }
+}
